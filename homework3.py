0b79b7a05c278a1018ebc776f9360dadc64cfe03
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-04 02:08:20 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,7 +1,7 @@
 name='Надо думать головой'
 print(name)
 print(name.upper())
-print(name.lower())
+print(name.upper().lower())
 print(name[0])
 print(name[-1])
 print('Надо думать головой' .replace(' ',''))
