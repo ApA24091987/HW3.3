9cb23dacefacae27458dd97f737e989bb0a20d0f
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-04 22:03:59 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,7 +1,8 @@
-name='Надо думать головой'
-print(name)
-print(name.upper())
-print(name.upper().lower())
-print(name[0])
-print(name[-1])
+my_string='Надо думать головой'
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string[-1])
 print('Надо думать головой' .replace(' ',''))
